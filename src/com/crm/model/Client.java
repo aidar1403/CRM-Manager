e643990e5c7ae9825e117ba7448da289ec243b92
aidar1403 <aidar9976@icloud.com>
2026-03-24 23:13:58 +0600

    Complete Client class with validation

---
+++ src/com/crm/model/Client.java
@@ -0,0 +1,42 @@
+package com.crm.model;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client extends Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static int nextId = 1;
+
+    private int id;
+    private String company;
+    private List<String> interactions;
+
+    public Client(String name, String email, String phone, String company) {
+        super(name, email, phone);
+        this.id = nextId++;
+        this.company = company;
+        this.interactions = new ArrayList<>();
+    }
+
+    public int getId(){ return id;}
+    public String getCompany() { return company;}
+    public List<String> getInteractions() {return interactions;}
+
+    public void setCompany(String company) {
+        this.company = (company!= null)?company: "";}
+
+    public void addInteraction(String interaction) {
+        if (interaction != null && !interaction.trim().isEmpty()) {
+            interactions.add(interaction);
+        }
+    }
+
+
+    @Override
+    public String getDisplayInfo() {
+        return "ID: " + id + " | " + getName() + " | " + getEmail() + " | " + company;
+    }
+}
+
