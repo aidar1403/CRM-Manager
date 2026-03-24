e26f9056732c0c0ae210bfe295ee8190e82d3c13
aidar1403 <aidar9976@icloud.com>
2026-03-24 21:59:55 +0600

    Add Person class

---
+++ src/com/crm/model/Person.java
@@ -0,0 +1,27 @@
+package com.crm.model;
+
+import java.io.Serializable;
+
+public abstract class Person implements Serializable{
+private static final long serialVersionUID = 1L;
+
+private String name;
+private String email;
+private String phone;
+
+public Person(String name,String email,String phone) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+}
+
+public String getName(){return name;}
+    public String getEmail(){return email;}
+    public String getPhone(){return phone;}
+
+    public void setName(String name){this.name = name;}
+    public void setEmail(String email){this.email = email;}
+    public void setPhone(String phone){this.phone = phone;}
+
+    public abstract String getDisplayInfo();
+}
